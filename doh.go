362f7955b0b77a7fb2870244fd37c3f3559afbf0
koomox <koomoxs@gmail.com>
2021-04-05 13:34:08 +0800

    Update doh.go

--- doh.go
+++ doh.go
@@ -9,12 +9,39 @@ import (
 	"net/http"
 	"net/url"
 	"time"
+	"strings"
+)
+
+const (
+	defaultTimeout = 5 * time.Second
 )
 
 var (
 	seed           = time.Now().UnixNano()
-	defaultTimeout = 5 * time.Second
+	doh_json_api_url = map[string][]string {
+		"cloudflare": {
+			"https://1.1.1.1/dns-query",
+			"https://1.0.0.1/dns-query",
+		},
+		"google": {
+			"https://8.8.8.8/resolve",
+			"https://8.8.4.4/resolve",
+		},
+		"quad9": {
+			"https://9.9.9.9:5053/dns-query",
+			"https://9.9.9.10:5053/dns-query",
+			"https://9.9.9.11:5053/dns-query",
+		},
+	}
+
+	dohJSONAPI []string
 )
+
+func init() {
+	for i, _ := range doh_json_api_url {
+		dohJSONAPI = append(dohJSONAPI, doh_json_api_url[i]...)
+	}
+}
 
 // Question is dns query question
 type Question struct {
@@ -43,32 +70,23 @@ type Response struct {
 }
 
 func newClient() *http.Client {
-	transport := &http.Transport{
-		IdleConnTimeout:       defaultTimeout,
-		DisableCompression:    true,
-		TLSHandshakeTimeout:   defaultTimeout,
-		ResponseHeaderTimeout: defaultTimeout,
-		ExpectContinueTimeout: defaultTimeout,
-		DisableKeepAlives:     true,
+	return &http.Client{
+		Transport: &http.Transport{
+			IdleConnTimeout:       defaultTimeout,
+			DisableCompression:    true,
+			TLSHandshakeTimeout:   defaultTimeout,
+			ResponseHeaderTimeout: defaultTimeout,
+			ExpectContinueTimeout: defaultTimeout,
+			DisableKeepAlives:     true,
+		},
 	}
-
-	client := &http.Client{
-		Transport: transport,
-	}
-
-	return client
 }
 
-const (
-	CLOUDFLARE_DOH_URL = "https://1.1.1.1/dns-query?"
-	QUAD9_DOH_URL      = "https://9.9.9.9:5053/dns-query?"
-)
-
-func exchangeHTTPS(name string) (b []byte, err error) {
+func exchangeHTTPS(name, provider string) (b []byte, err error) {
 	var req *http.Request = nil
 	var resp *http.Response = nil
 
-	Url, err := url.Parse(CLOUDFLARE_DOH_URL)
+	Url, err := url.Parse(provider)
 	if err != nil {
 		return
 	}
@@ -97,8 +115,8 @@ func exchangeHTTPS(name string) (b []byte, err error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
-func Lookup(name string) (addrs []string, err error) {
-	r, err := exchangeHTTPS(name)
+func lookup(name, provider string) (addrs []string, err error) {
+	r, err := exchangeHTTPS(name, provider)
 	if err != nil {
 		return
 	}
@@ -117,4 +135,30 @@ func Lookup(name string) (addrs []string, err error) {
 	}
 
 	return
+}
+
+func Lookup(name string)(addrs []string, err error) {
+	msgQ := make(chan []string)
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	for i := 0; i < len(dohJSONAPI); i++ {
+		go func(name, provider string){
+			address, e := lookup(name, provider)
+			if e == nil {
+				cancel()
+				msgQ <- append([]string{"success"}, address...)
+			}
+		}(name, dohJSONAPI[i])
+	}
+	select {
+	case <-time.After(defaultTimeout):
+		cancel()
+		return nil, errors.New("time out i/o")
+	case msg := <-msgQ:
+		if strings.EqualFold(msg[0], "success") {
+			addrs = append(addrs, msg[1:]...)
+			return
+		}
+		return nil, errors.New(msg[0])
+	}
 }
